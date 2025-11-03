c81566bce52ae80db03febfcafdf611f524595a5
Nicolas <zhangnicolas0@gmai.com>
2025-11-03 18:20:21 -0300

    Eliminacion de codigo innecesario

--- src/main/java/com/f1ruta/domain/Circuito.java
+++ src/main/java/com/f1ruta/domain/Circuito.java
@@ -2,10 +2,8 @@ package com.f1ruta.domain;
 
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.data.neo4j.core.schema.Relationship;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -17,10 +15,6 @@ public class Circuito {
     
     private double latitud;
     private double longitud;
-
-    // Relaciones hacia otros circuitos (aristas con propiedades)
-    @Relationship(type = "CONECTA_CON")
-    private List<Conexion> conexiones;
 
     public Circuito(String nombre, double latitud, double longitud) {
         this.nombre = nombre;
--- src/main/java/com/f1ruta/repository/CircuitoRepository.java
+++ src/main/java/com/f1ruta/repository/CircuitoRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CircuitoRepository extends Neo4jRepository<Circuito, String> {
-    
-    Optional<Circuito> findByNombre(String nombre);
     
     List<Circuito> findAll();
 }
